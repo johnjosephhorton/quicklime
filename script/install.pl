945986cd9763623d7148f9653d271c92f4376d13
paolo <paolo@blacksheep.local>
2012-05-22 12:00:55 +0200

    Updated *.deb dependencies

--- script/install.pl
+++ script/install.pl
@@ -1,8 +1,26 @@
 #! /usr/bin/perl
 
+# Paolo Lulli 2012
+
 use lib qw(lib);
 use LimeSurvey;
+use AptCaller;
 
-&getLimeSurvey();
+my @dependencies = (    'mysql-client', 
+			'mysql-server', 
+			'apache2', 
+			'php5', 
+			'php5-mysql', 
+			'php-cli');
 
+
+&getLimeSurvey(); # Downloads the code from LimeSurvey website
+
+#&aptInstall("<package-name>");
+
+foreach my $dep (@dependencies){
+	print "Installing package: [".$dep."]\n";
+	&aptInstall($dep);
+	print "DONE\n";
+}
 
