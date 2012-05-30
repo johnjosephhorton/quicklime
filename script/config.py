dcd3f3ea29408ff34d97292fb4ca920a5c7a48d5
paolo <paolo@blacksheep.local>
2012-05-30 13:07:06 +0200

    moved url and namefile into config.py

--- script/config.py
+++ script/config.py
@@ -1,4 +1,9 @@
+# Download URL of the limesurvey *.tar.gz 
 download_url='http://www.limesurvey.org/en/stable-release/finish/25-latest-stable-release/457-limesurvey192plus-build120530zip'
+
+#  file_name should contain the name of the *.tar.gz archive downloaded from the internet
+file_name = 'limesurvey.tar.gz'
+
 email_account='youremail@example.com'
 email_password='custom_password'
 siteadminbounce=''
